61764584ebfec4654d4782584180dde18d1a87ad
launc <toshadrazdov@gmail.com>
2026-04-19 18:05:06 +0300

    refactor: optimized food generation

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-export const fieldSize = 15
+export const fieldSize = 10
 export let vars = {
     running: true,
     count: 0
@@ -13,7 +13,7 @@ export let snake = {
         snake.parts = Array.from(Array(length)).map((_, i) => {return {x: i, y: y}})
     }
 }
-export let food = {x: Math.round(fieldSize - fieldSize / 4), y: Math.round(fieldSize / 2)}
+export let food = {}
 
 export const moveSnake = () => {
     const directions = {
@@ -23,22 +23,28 @@ export const moveSnake = () => {
         right: {x: 1, y: 0}
     }
 
-    const x = snake.parts.at(-1).x + directions[snake.direction].x
-    const y = snake.parts.at(-1).y + directions[snake.direction].y
+    const [x, y] = [snake.parts.at(-1).x + directions[snake.direction].x,
+                    snake.parts.at(-1).y + directions[snake.direction].y]
     snake.parts.push({x: x, y: y})
+
     if (checkFoodCollision()) {
         vars.count += 1
-        generateFood()
+        food = generateFoodPos()
     } else {snake.parts.shift()}
+}
+
+
+export const checkWin = () => {
+    return snake.parts.length === fieldSize ** 2
 }
 
 export const checkCollision = () => {
     const head = snake.parts.at(-1)
     const bodyCollision = snake.parts.slice(0, -1).some((part) => {return part.x === head.x && part.y === head.y})
     return (
-        (head.x < 0 || head.x > fieldSize - 1) 
-        || (head.y < 0 || head.y > fieldSize - 1) 
-        || bodyCollision
+        (head.x < 0 || head.x > fieldSize - 1) || 
+        (head.y < 0 || head.y > fieldSize - 1) || 
+        bodyCollision
     )
 }
 
@@ -47,24 +53,24 @@ export const checkFoodCollision = () => {
     return head.x === food.x && head.y === food.y
 }
 
-const getRandomCoord = () => {
-    return Math.max(0, Math.round(Math.random() * fieldSize - 1))
-}
+export const generateFoodPos = () => {
+    const emptyCells = []
+    for (let y = 0; y < fieldSize; y++) {
+        for (let x = 0; x < fieldSize; x++) {
+            const hasSnakePart = snake.parts.some((part) => {return part.x === x && part.y === y})
+            if (!hasSnakePart) {emptyCells.push({x: x, y: y})}
+        }
+    }
 
-export const generateFood = () => {
-    let x = getRandomCoord(1)
-    let y = getRandomCoord(1)
-
-    const hasCollision = snake.parts.some((part) => {
-        return part.x === x && part.y === y
-    })
-    
-    if (hasCollision) {generateFood()} else {food = {x: x, y: y}}
+    if (emptyCells.length) {
+        const randomCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+        return randomCell
+    }
 }
 
 export const initGame = () => {
     snake.init(3)
-    food = {x: Math.round(fieldSize - fieldSize / 4), y: Math.round(fieldSize / 2)}
+    food = generateFoodPos()
     vars.count = 0
     vars.running = true
 }
